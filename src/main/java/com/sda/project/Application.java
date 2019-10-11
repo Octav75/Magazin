b0ca215a4f210dcafa3b1581deb8d0602aae2c3f
Octav75 <OctavSwDev.gmail.com>
2019-10-11 21:35:24 +0300

    Set up spring boot application

--- src/main/java/com/sda/project/Application.java
+++ src/main/java/com/sda/project/Application.java
@@ -1,3 +1,8 @@
+package com.sda.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
