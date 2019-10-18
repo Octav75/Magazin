1300beb613b4946d6174cd42700bd019b44485fd
Octav75 <OctavSwDev.gmail.com>
2019-10-18 21:19:09 +0300

    "Saving name in DB"

--- src/main/java/com/sda/project/register/RegisterController.java
+++ src/main/java/com/sda/project/register/RegisterController.java
@@ -1,14 +1,34 @@
 package com.sda.project.register;
 
 import com.sda.project.users.User;
+import com.sda.project.users.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RegisterController {
 
+    private UserService userService;
+
+    @Autowired
+    public RegisterController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/register")
-    public String showSignUpForm(User user) {
+    public String showSignUpForm(Model model) {
+        User u = new User();
+        model.addAttribute("user", u);
         return "register";
+    }
+
+    @PostMapping("/register")
+    public String postRegister(User user) {
+        userService.save(user);
+
+        return "index";
     }
 }
--- src/main/java/com/sda/project/users/User.java
+++ src/main/java/com/sda/project/users/User.java
@@ -4,7 +4,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.NotBlank;
 
 @Entity
 public class User {
@@ -13,13 +12,8 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
-    @NotBlank(message = "Name is mandatory")
     private String name;
-
-    @NotBlank(message = "Email is mandatory")
     private String email;
-
-    @NotBlank(message = "Password is mandatory")
     private String password;
 
     public long getId() {
