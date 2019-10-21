0acbc1dcffe1ab815b717444f9abdc07d666c3e1
Octav75 <OctavSwDev.gmail.com>
2019-10-21 19:03:46 +0300

    "Authorizeed Login"

--- src/main/java/com/sda/project/users/UserService.java
+++ src/main/java/com/sda/project/users/UserService.java
@@ -1,19 +1,33 @@
 package com.sda.project.users;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Optional;
 
-@Component
-public class UserService{
+@Service("userService")
+public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
 
     @Autowired
     public UserService(UserRepository userRepository) {this.userRepository = userRepository;}
 
     public void save(User user) {
+
+        String oldPassword = user.getPassword();
+        user.setPassword(passwordEncoder.encode(oldPassword));
         userRepository.save(user);
     }
 
@@ -21,6 +35,21 @@ public class UserService{
         return userRepository.findById(id);
     }
 
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> byEmail = userRepository.findByEmail(email);
+        User user;
+        if(byEmail.isPresent()) {
+            user=byEmail.get();
+        }else {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), getAuthority(user));
+    }
+
+    private Collection<? extends GrantedAuthority> getAuthority(User user) {
+        return Arrays.asList(new SimpleGrantedAuthority("ROLE_" + "USER")); //TODO add role field to user; can be user, admin etc.
+    }
 }
 
 
